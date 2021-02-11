27a61146e632678332ef4b7df0e388e2ff4f87af
EMarshall <emilyjmarshall121@gmail.com>
2021-02-11 10:49:32 -0700

    added conditional logic

--- app.js
+++ app.js
@@ -39,14 +39,19 @@ const promptUser = () => {
         }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to add an About section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
       message: 'Provide some information about yourself: (Required!)',
-      validate: nameInput => {
-        if (nameInput) {
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
           return true;
         } else {
-          console.log('Please enter information about yourself!');
           return false;
         }
         }
